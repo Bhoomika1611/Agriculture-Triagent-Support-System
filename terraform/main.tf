79c80a0be88e68a16b01e46f0f0ffb2c48ec4245
Bhoomika Patidar <bhoomikapatidar2@gmail.com>
2026-04-17 19:33:48 +0530

    update terraform and deploy config for ubuntu EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,26 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "agri_server" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu AMI (valid in us-east-1)
   instance_type = "m7i-flex.large"
 
   key_name = "agri-key"
 
   root_block_device {
-    volume_size = 20
-    volume_type = "gp2"
+    volume_size = 30
+    volume_type = "gp3"
   }
 
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
-              apt install docker.io -y
+              apt install -y docker.io
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
               EOF
 
   tags = {
-    Name = "Agri-Server"
+    Name = "Agri-Server-Ubuntu"
   }
 }
